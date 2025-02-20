07190ace66cb88ca698dc667f7c8126b84961fc3
Karl-Gustav Kallaste <kgkallaste@gmail.com>
2025-02-20 20:14:01 +0200

    Fix api -> db connection.

--- Movies.Host/Program.cs
+++ Movies.Host/Program.cs
@@ -3,11 +3,14 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgresDb = builder.AddPostgres("default")
+var pgUser = builder.AddParameter("PgUser", secret: true);
+var pgPassword = builder.AddParameter("PgPass", secret: true);
+
+var postgres = builder.AddPostgres(name: "default", port: 5432, userName: pgUser, password: pgPassword)
     .WithLifetime(ContainerLifetime.Persistent);
 
 builder.AddProject<Movies_Api>("Api")
-    .WithReference(postgresDb)
-    .WaitFor(postgresDb)
+    .WithReference(postgres)
+    .WaitFor(postgres)
     .WithEnvironment("ConnectionStrings__Default", builder.Configuration.GetConnectionString("Default"));
 builder.Build().Run();
